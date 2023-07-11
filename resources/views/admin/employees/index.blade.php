721849a6f1ef43a0c88feabe477b75118c70a107
Mohsin <abdul.mohsin.malik@gmail.com>
2023-07-11 15:18:24 +0500

    config files

--- resources/views/admin/employees/index.blade.php
+++ resources/views/admin/employees/index.blade.php
@@ -20,7 +20,7 @@
                             <span>{{ $message }}</span>
                         </div>
                     @endif
-                    
+
                     <div class="flex flex-col">
                         <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                             <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
